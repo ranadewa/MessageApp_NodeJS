29e8b022433bf516d0e7f554456349e7178f354f
Ranadewa <thisarar@lseg.stockex.local>
2018-02-10 17:05:20 +0530

    Create post message service

--- DemoAppChatClientForBrowser/server.js
+++ DemoAppChatClientForBrowser/server.js
@@ -1,15 +1,23 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
+var messages = [ 
     {name: 'Tim', message: 'Hi'},
     {name: 'Jane', message: 'Hello'}
 ]
 
 app.get('/messages', (req, res) =>{
     res.send(messages)
+})
+
+app.post('/messages', (req, res) =>{
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 var server = app.listen(3000, () => {
