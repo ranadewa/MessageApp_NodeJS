ec1ed5dbe92082cb42b445980346d3720258c7aa
Ranadewa <thisarar@lseg.stockex.local>
2018-02-10 18:00:20 +0530

    Adding Mongo DB to save previous chats

--- DemoAppChatClientForBrowser/server.js
+++ DemoAppChatClientForBrowser/server.js
@@ -3,28 +3,48 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoos = require('mongoose')
 
+var dbUrl = 'mongodb://user:user@ds129428.mlab.com:29428/node-js-learning'
+
+var Message = mongoos.model('Message', {
+  name:String,
+  message:String
+})
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'}
-]
-
 app.get('/messages', (req, res) =>{
-    res.send(messages)
+
+  Message.find({}, (err, messages) =>{
+      res.send(messages)
+  })
+
 })
 
 app.post('/messages', (req, res) =>{
-    messages.push(req.body)
+
+  var message = new Message(req.body)
+
+  message.save((err) => {
+
+    if(err)
+      sendStatus(500)
+
+    
     io.emit('message', req.body)
     res.sendStatus(200)
+  })
+
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoos.connect(dbUrl, (err) =>{
+  console.log('Mongo connection ' , err)
 })
 
 var server = http.listen(3000, () => {
