a3c929bc2f505a3c33a49ddc65987fc0d4c8ca77
Ranadewa <thisarar@lseg.stockex.local>
2018-02-10 17:37:36 +0530

    Socket io for chat clients

--- DemoAppChatClientForBrowser/server.js
+++ DemoAppChatClientForBrowser/server.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [ 
+var messages = [
     {name: 'Tim', message: 'Hi'},
     {name: 'Jane', message: 'Hello'}
 ]
@@ -17,9 +19,14 @@ app.get('/messages', (req, res) =>{
 
 app.post('/messages', (req, res) =>{
     messages.push(req.body)
+    io.emit('message', req.body)
     res.sendStatus(200)
 })
 
-var server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
+var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
